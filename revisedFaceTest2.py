24e9f5b7beee0565f78973127356482da7cf0ee4
awesomepranav <awesomepranav02@gmail.com>
2020-03-29 20:20:14 -0700

    Face project files

---
+++ revisedFaceTest2.py
@@ -0,0 +1,30 @@
+import io
+import os
+from google.cloud import vision
+def detect_faces(path):
+    #"""Detects faces in an image."""	
+
+    client = vision.ImageAnnotatorClient()
+	# The name of the image file to annotate
+    file_name = os.path.join('.', 'Surprised.jpg')
+	# Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+	#print('file opened')
+    image = vision.types.Image(content=content)
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+	# Names of likelihood from google.cloud.vision.enum
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY','POSSIBLE',
+                    'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+    for face in faces:
+     		print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+     		print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+     		print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+     		vertices = (['({},{})'.format(vertex.x, vertex.y)
+                for vertex in face.bounding_poly.vertices])
+
+     		print('face bounds: {}'.format(','.join(vertices)))
+detect_faces("/home/pi/pranav/Sndar.jpg")
